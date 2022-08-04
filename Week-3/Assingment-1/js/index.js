639f4bbf1de0ff64735b02a8c9f8bed517f11637
bhushanhr26 <58306435+bhushanhr26@users.noreply.github.com>
2022-08-04 23:12:03 +0530

    Memoized Function Created for sum

---
+++ Week-3/Assingment-1/js/index.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a+b
+}
+const  memoize=(fn)=>{
+    let cache = {};
+    return (a,b)=>{
+        if (a,b in cache){
+            console.log('cache');
+            return cache[a,b]
+        }
+        else {
+            let result = fn(a,b);
+            cache[a,b] = result;
+            return result
+        }
+    }
+}
+const memoizeAdd = memoize(add)
+console.log(memoizeAdd(2,5))
