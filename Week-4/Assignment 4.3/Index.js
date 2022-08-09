917939a10f9ebb8272b1498a2355cd9ce4a1d967
bhushanhr26 <58306435+bhushanhr26@users.noreply.github.com>
2022-08-10 00:33:21 +0530

    one more console added for asssignment 4.3

--- Week-4/Assignment 4.3/Index.js
+++ Week-4/Assignment 4.3/Index.js
@@ -3,6 +3,7 @@ let n1 = 0;
 let n2 = 1;
 let result = 0 ;
 for(let i =0;i<n;i++){
+    //console.log("The Fibonacci Series is:")
     console.log(n1)
     result = n1 + n2;
     n1 = n2;
@@ -12,6 +13,6 @@ for(let i =0;i<n;i++){
 return result
 }
 console.log(fibonacciSeries(5))
-console.log(fibonacciSeries(6))
-console.log(fibonacciSeries(2))
-console.log(fibonacciSeries(10))
+// console.log(fibonacciSeries(6))
+// console.log(fibonacciSeries(2))
+// console.log(fibonacciSeries(10))
