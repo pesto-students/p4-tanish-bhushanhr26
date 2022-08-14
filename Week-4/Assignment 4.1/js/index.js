76e787cd13cc7c84eb93b9a6edb4b3337aa4cf6a
bhushanhr26 <58306435+bhushanhr26@users.noreply.github.com>
2022-08-14 13:17:33 +0530

    Assignment 4.1 completed

---
+++ Week-4/Assignment 4.1/js/index.js
@@ -0,0 +1,20 @@
+function getNumber() {
+  let x = Math.random();
+  let y = x.toFixed(2)
+  return new Promise(function (reject, resolve) {
+    if (y/5===0) {
+      reject("It is Divisible by 5");
+      return;
+    }
+
+    resolve(y);
+  });
+}
+getNumber()
+  .then(function (result) {
+    console.log(`success ${result}`);
+  })
+  .catch(function (error) {
+    console.log("error");
+    console.log(error);
+  });
