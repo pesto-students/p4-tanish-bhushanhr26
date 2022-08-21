7b7103338fe13699bc6a445e98dc621f25ee7fa0
bhushanhr26 <58306435+bhushanhr26@users.noreply.github.com>
2022-08-22 00:41:13 +0530

    Assignment-1 completed

--- Week-5/Assignment-1/index.js
+++ Week-5/Assignment-1/index.js
@@ -1,20 +1,3 @@
-// // generator function
-// function* generatorFunc() {
-
-//     console.log("1. code before the first yield");
-//     yield 100;
-
-//    console.log("2. code before the second yield");
-//     yield 200;
-// }
-
-// // returns generator object
-// const generator = generatorFunc();
-
-// console.log(generator.next());
-// console.log(generator.next());
-
-
 function* generator() {
   yield doTask1();
   yield doTask2();
