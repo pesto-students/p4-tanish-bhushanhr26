c07e66db4c3af31693c2dc55c35ba3338efd0c50
bhushanhr26 <58306435+bhushanhr26@users.noreply.github.com>
2022-11-02 22:30:50 +0530

    CSS added for assignemnt 2

--- Week-11/redux-app/src/App.js
+++ Week-11/redux-app/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { reset, increment } from "./feature/slice";
-
+import "./App.css";
 function App() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
   return (
     <div className="App">
-      <button onClick={() => dispatch(reset())}>Reset</button>
-      <span>{count}</span>
-      <button onClick={() => dispatch(increment())}> + </button>
+      <span>{`You have walked ${count} steps today`}</span>
+      <button onClick={() => dispatch(increment())} className="addBtn">
+        {" "}
+        Add a step{" "}
+      </button>
+      <button onClick={() => dispatch(reset())} className="resetBtn">
+        Reset Steps
+      </button>
     </div>
   );
 }
